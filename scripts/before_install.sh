9ccb52db550a1f9aa26eed771585e87ff9cb378c
diego <diego.mreeyes@gmail.com>
2022-10-24 22:31:23 -0400

    adding nvm install command to the before_install script

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,6 +4,7 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 echo "Nvm installation finished"
+nvm install 16
 nvm use 16
 echo "nvm use command finished"
 
