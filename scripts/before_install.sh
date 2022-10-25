3beca03d2e00a71be2c6a1188941df9d5f63d893
diego <diego.mreeyes@gmail.com>
2022-10-24 21:25:32 -0400

    updating commands on before_installing script

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,7 +3,9 @@
 #download node and npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install node
+echo "Nvm installation finished"
+nvm use 16
+echo "nvm use command finished"
 
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/baseball-website"
